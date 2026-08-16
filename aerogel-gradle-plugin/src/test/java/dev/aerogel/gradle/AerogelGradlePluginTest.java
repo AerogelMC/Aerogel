0846d4d7274dfcf76f46aee9b5ecb352227507ed
마카롱 <76113903+Mkkas3145@users.noreply.github.com>
2026-08-17 00:54:41 +0900

    Separate tests from Maven publishing

--- aerogel-gradle-plugin/src/test/java/dev/aerogel/gradle/AerogelGradlePluginTest.java
+++ aerogel-gradle-plugin/src/test/java/dev/aerogel/gradle/AerogelGradlePluginTest.java
@@ -107,10 +107,6 @@ class AerogelGradlePluginTest {
             </project>
             """);
         Files.writeString(project.resolve("settings.gradle"), "rootProject.name = 'sample'\n");
-        Files.writeString(project.resolve("gradle.properties"), """
-            org.gradle.daemon=false
-            kotlin.compiler.execution.strategy=in-process
-            """);
         Files.writeString(project.resolve("build.gradle"), """
             plugins {
                 id 'dev.aerogel.plugin'
